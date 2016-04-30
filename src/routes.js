fd7993934ab4e500c39e9061c312412dd9f44a3d
Ian Brennan <ian@barbershop.io>
2016-04-30 13:48:52 -0600

    routes and dependencies and home page

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ import { Route } from 'react-router';
 import App from './containers/App';
 import About from './components/About';
 import ListPage from './containers/ListPage';
+import Home from './containers/Home/Home';
 
 export default (
   <Route path='/' component={App}>
     <Route path='/about' component={About} />
     <Route path='/list' component={ListPage} />
+    <Route path='/home' conponent={Home} />
   </Route>
 );
